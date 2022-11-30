e174d895fcdee56e8240fbfb6d1a6a26aca30f5a
Vivek <vivek.chetia@clinifyhealth.com>
2022-11-30 11:57:09 +0530

    updated check conditions

--- YourRideIsHere.ts
+++ YourRideIsHere.ts
@@ -1,96 +1,98 @@
-import { createWriteStream } from 'fs'
-type ActionType = 'INPUT_1' | 'INPUT_2'
-type ResultType = 'GO' | 'STAY' | 'NA'
+import { createWriteStream } from "fs";
+type ActionType = "INPUT_1" | "INPUT_2";
+type ResultType = "GO" | "STAY" | "NA";
 
 interface ResultPairs {
-    input1: string | undefined;
-    input2: string | undefined;
-    result: ResultType
+  input1: string | undefined;
+  input2: string | undefined;
+  result: ResultType;
 }
 
 class YourRideIsHere {
-    #result: ResultPairs[] = []
-    #action_tracker: ResultPairs & { action: ActionType } = {
-        action: 'INPUT_1',
-        input1: undefined,
-        input2: undefined,
-        result: 'NA'
+  #result: ResultPairs[] = [];
+  #action_tracker: ResultPairs & { action: ActionType } = {
+    action: "INPUT_1",
+    input1: undefined,
+    input2: undefined,
+    result: "NA",
+  };
+
+  #resetActionTracker() {
+    this.#action_tracker = {
+      action: "INPUT_1",
+      input1: undefined,
+      input2: undefined,
+      result: "NA",
+    };
+  }
+
+  #getCharPosition(char: string) {
+    const charAscii = char.toUpperCase().charCodeAt(0);
+    if (charAscii > 64 && charAscii < 91) return charAscii - 64;
+  }
+
+  #getResult(x: string, y: string): ResultType {
+    let xProducts = 1;
+    let yProducts = 1;
+
+    for (let i = 0; i < x.length; i++) {
+      xProducts *= this.#getCharPosition(x[i]);
     }
 
-    #resetActionTracker() {
-        this.#action_tracker = {
-            action: 'INPUT_1',
-            input1: undefined,
-            input2: undefined,
-            result: 'NA'
-        }
+    for (let j = 0; j < y.length; j++) {
+      yProducts *= this.#getCharPosition(y[j]);
     }
 
-    #getCharPosition(char: string) {
-        const charAscii = char.toUpperCase().charCodeAt(0);
-        if (charAscii > 64 && charAscii < 91) return charAscii - 64;
+    if (xProducts % 47 === yProducts % 47) {
+      return "GO";
     }
 
-    #getResult(x: string, y: string): ResultType {
-        let xProducts = 1;
-        let yProducts = 1;
-
-        for (let i = 0; i < x.length; i++) {
-            xProducts *= this.#getCharPosition(x[i]);
-        }
-
-        for (let j = 0; j < y.length; j++) {
-            yProducts *= this.#getCharPosition(y[j]);
-        }
-
-
-        if (xProducts % 47 === 27 && yProducts % 47 === 27) {
-            return 'GO'
-        }
-
-        if (xProducts % 47 !== 27 && yProducts % 47 !== 27) {
-            return 'STAY'
-        }
-
-        return 'NA'
+    if (xProducts % 47 !== yProducts % 47) {
+      return "STAY";
     }
 
-    calculate(input: string) {
-        if (this.#action_tracker.action === 'INPUT_1') {
-            this.#action_tracker.input1 = input;
-            this.#action_tracker.action = 'INPUT_2'
-            return
-        }
+    return "NA";
+  }
 
-        if (this.#action_tracker.action === 'INPUT_2') {
-            this.#action_tracker.input2 = input;
-            this.#action_tracker.result = this.#getResult(this.#action_tracker.input1, this.#action_tracker.input2)
-        }
-
-        this.#result.push(this.#action_tracker)
-        this.#resetActionTracker();
+  calculate(input: string) {
+    if (this.#action_tracker.action === "INPUT_1") {
+      this.#action_tracker.input1 = input;
+      this.#action_tracker.action = "INPUT_2";
+      return;
     }
 
-    getResult() {
-        const w_stream = createWriteStream('output.out')
-        for (let i = 0; i < this.#result.length; i++) {
-            const { input1, input2, result } = this.#result[i]
-            const out = `[${input1}, ${input2}] : ${result}`
-            console.log(out)
-            w_stream.write(result + '\n')
-        }
-
-        if (this.#action_tracker.input2 === undefined) {
-            const out = `[${this.#action_tracker.input1}, ''] : Incomplete input`
-            console.log(out)
-            w_stream.write('Incomplete input\n')
-            this.#resetActionTracker()
-        }
-
-        this.#result = []
-        w_stream.end()
-        w_stream.close()
+    if (this.#action_tracker.action === "INPUT_2") {
+      this.#action_tracker.input2 = input;
+      this.#action_tracker.result = this.#getResult(
+        this.#action_tracker.input1,
+        this.#action_tracker.input2
+      );
     }
+
+    this.#result.push(this.#action_tracker);
+    this.#resetActionTracker();
+  }
+
+  getResult() {
+    const w_stream = createWriteStream("output.out");
+    for (let i = 0; i < this.#result.length; i++) {
+      const { input1, input2, result } = this.#result[i];
+      const out = `[${input1}, ${input2}] : ${result}`;
+      console.log(out);
+      w_stream.write(result + "\n");
+    }
+
+    if (this.#action_tracker.input2 === undefined) {
+      const out = `[${this.#action_tracker.input1}, ''] : Incomplete input`;
+      console.log(out);
+      w_stream.write("Incomplete input\n");
+      this.#resetActionTracker();
+    }
+
+    this.#result = [];
+    w_stream.end();
+    w_stream.close();
+  }
 }
 
 export default YourRideIsHere;
